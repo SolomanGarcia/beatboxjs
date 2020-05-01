c314152007de25d9e82cb98dc1a39d33b460fe62
Soloman Garcia <soloman.garcia@gmail.com>
2020-05-01 12:15:13 -0600

    debug

--- script.js
+++ script.js
@@ -55,7 +55,8 @@ triggerBeat = (event) => {
 
     if (keyCode in beats) {
         let keyPress = beats[keyCode];
-        keyPress.beat.play();
+        keyPress.beat.play()
+        keyPress.button.select();
     }
 }
 
